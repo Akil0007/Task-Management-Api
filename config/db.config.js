d70f06f066630d234034b26ed8dd849c9a791732
Akil0007 <139245385+Akil0007@users.noreply.github.com>
2025-01-29 12:11:42 +0530

    updated db config

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://akil07:9597550315@cluster0.h5kqj.mongodb.net/task-management?retryWrites=true&w=majority&appName=Cluster0",
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.h5kqj.mongodb.net/task-management?retryWrites=true&w=majority&appName=Cluster0`,
       { useNewUrlParser: true, useUnifiedTopology: true }
     );
     console.log("Database connected successfully");
